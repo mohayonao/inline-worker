bb950b95fe6cdbb557a6beb7c97b0459c7ebf3fc
mohayonao <mohayonao@gmail.com>
2016-07-02 12:35:29 +0900

    replace mocha -> eater

--- test/inline-worker.js
+++ test/inline-worker.js
@@ -3,22 +3,15 @@
 const assert = require("assert");
 const InlineWorker = require("../");
 
-describe("InlineWorker", () => {
-  describe("constructor(func: function, [ self: object ])", () => {
-    it("works", (done) => {
-      let worker = new InlineWorker(function(self) {
-        self.onmessage = function(e) {
-          assert(e.data === "hello");
-          self.postMessage("good bye");
-        };
-      });
-
-      worker.onmessage = (e) => {
-        assert(e.data === "good bye");
-        done();
-      };
-
-      worker.postMessage("hello");
-    });
-  });
+const worker = new InlineWorker(function(self) {
+  self.onmessage = function(e) {
+    assert(e.data === "hello");
+    self.postMessage("good bye");
+  };
 });
+
+worker.onmessage = (e) => {
+  assert(e.data === "good bye");
+};
+
+worker.postMessage("hello");
